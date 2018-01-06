b0cd875cc878e99d7fa804749f27fc3d610aeee5
simon <simonireilly@gmail.com>
2018-01-06 13:58:55 +0000

    Customised the search in administrate dashboard

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -1,57 +1,35 @@
 require "administrate/base_dashboard"
 
 class PostDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
-    id: Field::Number,
-    date: Field::DateTime,
-    rationale: Field::Text,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    user: Field::BelongsTo.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    date: Field::DateTime.with_options(searchable: false),
+    rationale: Field::Text.with_options(searchable: true),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = [
-    :user,
-    :id,
-    :date,
-    :rationale,
+  COLLECTION_ATTRIBUTES = %i[
+    user
+    id
+    date
+    rationale
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = [
-    :user,
-    :id,
-    :date,
-    :rationale,
-    :created_at,
-    :updated_at,
+  SHOW_PAGE_ATTRIBUTES = %i[
+    user
+    id
+    date
+    rationale
+    created_at
+    updated_at
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = [
-    :user,
-    :date,
-    :rationale,
+  FORM_ATTRIBUTES = %i[
+    user
+    date
+    rationale
   ].freeze
 
-  # Overwrite this method to customize how posts are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(post)
-  #   "Post ##{post.id}"
-  # end
 end
