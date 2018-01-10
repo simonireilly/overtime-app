79df3c274fb89d49abbe214c28dc642a4bf4e8c8
simon <simonireilly@gmail.com>
2018-01-10 06:50:01 +0000

    Implemented labels with stats values on labels for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+    bootstrap_classes = { approved: 'success', submitted: 'primary', rejceted: 'danger' }
+    content_tag(:span, status.titleize, class: "label label-#{bootstrap_classes[status.to_sym]}")
+  end
 end
