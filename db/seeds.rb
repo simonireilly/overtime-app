53a8def51b36845ca054b871726d03f04e800c6d
simon <simonireilly@gmail.com>
2018-01-11 07:29:56 +0000

    Implemented the auditlog seeds and factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,27 @@
-@user = User.create(email: 'user@test.com',
+@user = User.create!(email: 'user@test.com',
                     password: 'asdfasdf',
                     password_confirmation: 'asdfasdf',
                     first_name: 'John',
                     last_name: 'Snow',
-                    phone: '+447522285748')
+                    phone: '07522285748')
 
 puts '-- One user created'
 
-AdminUser.create(email: 'admin@test.com',
+AdminUser.create!(email: 'admin@test.com',
                  password: 'asdfasdf',
                  password_confirmation: 'asdfasdf',
                  first_name: 'Admin',
                  last_name: 'Name',
-                 phone: '+447522285748')
+                 phone: '07522285748')
 
 puts '-- One admin_user created'
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5 )
 end
-
 puts '-- 100 posts have been created'
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today() - 6.days),  end_date: nil )
+end
+puts '-- 100  have been created'
--- spec/controllers/audit_logs_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AuditLogsController, type: :controller do
-
-end
