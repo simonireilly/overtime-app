24bd5cc60cb8213d3d754b8d9183a68c5e90291d
simon <simonireilly@gmail.com>
2018-01-12 07:18:28 +0000

    implemented enum for status and application helper for status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def active?(path)
     'active' if current_page?(path)
   end
+
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+    bootstrap_classes = { approved: 'success',
+                          submitted: 'primary',
+                          rejected: 'danger',
+                          pending: 'primary',
+                          confirmed: 'success' }
+    content_tag(:span, status.titleize, class: "label label-#{bootstrap_classes[status.to_sym]}")
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,2 @@
 module PostsHelper
-  def status_label status
-    status_span_generator status
-  end
-
-  private
-
-  def status_span_generator status
-    bootstrap_classes = { approved: 'success', submitted: 'primary', rejected: 'danger' }
-    content_tag(:span, status.titleize, class: "label label-#{bootstrap_classes[status.to_sym]}")
-  end
 end
